93e811a7146d19dd5c4cebedd082768fe31516bc
紀　祐司 <signtheswish@gmail.com>
2017-01-04 18:57:13 +0900

    GoForwardUnityChanプロジェクトのコミット
    課題であるキューブの接触時に効果音を追加しました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	//キューブの移動速度
+	private float speed = -0.2f;
+
+	//消滅位置
+	private float deadLine = -10;
+
+
+	private AudioSource block;
+
+	public AudioClip sound;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		block = gameObject.AddComponent<AudioSource> ();
+		block.clip = sound;
+		block.volume = 3;
+		block.loop = false;
+
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//キューブを移動させる
+		transform.Translate(this.speed,0,0);
+
+		//画面外に出たら破棄する
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+
+		}
+	
+	}
+
+
+	void OnCollisionEnter2D (Collision2D collision){
+
+		if (collision.gameObject.tag == "GroundTag") {
+		
+			block.Play ();
+
+		}
+
+		if (collision.gameObject.tag == "CubeTag") {
+
+			block.Play ();
+
+		}
+
+	}
+
+
+		
+
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour {
+
+
+	//アニメーションするためのコンポーネントを入れる
+	Animator animator;
+
+	//Unityちゃんを移動させるコンポーネントを入れる
+	Rigidbody2D rigid2D;
+
+
+	//地面の位置
+	private float groundLevel =-3.0f;
+
+	//ジャンプの速度の減衰
+	private float dump = 0.8f;
+
+	//ジャンプの速度
+	private float jumpVelocity = 20.0f;
+
+	//ゲームオーバーになる位置
+	private float deadLine = -9.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		//アニメータのコンポーネントを取得
+		this.animator = GetComponent<Animator>();
+
+		//Rigidbody2Dのコンポーネントを取得
+		this.rigid2D = GetComponent<Rigidbody2D>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//走るアニメーションを再生するために、Animatorのパラメータを調整する
+		this.animator.SetFloat("Horizontal", 1);
+
+
+		//着地しているかどうか調べる
+		bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+		this.animator.SetBool ("isGround", isGround);
+
+
+		//ジャンプ状態の時にボリュームを0にする
+		GetComponent<AudioSource>().volume = (isGround)? 1: 0;
+
+
+
+		//着地でクリックされた場合
+		if (Input.GetMouseButtonDown (0) && isGround) {
+			//上方向に力をかける
+			this.rigid2D.velocity = new Vector2 (0,this.jumpVelocity);
+
+		}
+
+		//クリックをやめたら上方向への速度を減速する
+		if (Input.GetMouseButton (0) == false) {
+			if (this.rigid2D.velocity.y > 0) {
+				this.rigid2D.velocity *= this.dump;
+			}
+
+		}
+
+		//デッドラインを超えたらゲームオーバーにする
+
+		if (transform.position.x < this.deadLine) {
+			//UIControllerのGameOver関数を呼び出して画面上に表示する
+			GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+
+			//Unityちゃんを破棄する
+			Destroy(gameObject);
+
+
+
+
+		}
+
+
+
+	
+
+	}
+}
